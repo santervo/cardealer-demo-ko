ee2877eb121f96fc95844a28a92ba8d6dca159a8
Santtu Lintervo <santervo@gmail.com>
2012-04-03 23:01:28 +0300

    simplified tests

--- test/unit/car_test.rb
+++ test/unit/car_test.rb
@@ -1,58 +1,8 @@
 require 'test_helper'
 
 class CarTest < ActiveSupport::TestCase
-	setup do
-		@valid_attrs = { :licenceNumber => "XYZ-123", :model => "Honda", :yearModel => 2010, :price => 5000.0 }
-	end
-
-	context "car with valid attributes" do
-		setup do
-			@car = Car.new @valid_attrs
-		end
-
-		should "save" do
-			assert @car.save
-		end
-	end
-
-	context "car without licence number" do
-		setup do
-			@car = Car.new @valid_attrs.merge(:licenceNumber => "")
-		end
-
-		should "not save" do
-			assert !@car.save
-		end
-	end
-
-	context "car without model" do
-		setup do
-			@car = Car.new @valid_attrs.merge(:model => "")
-		end
-
-		should "not save" do
-			assert !@car.save
-		end
-	end
-
-
-	context "car without yearModel" do
-		setup do
-			@car = Car.new @valid_attrs.merge(:yearModel => "")
-		end
-
-		should "not save" do
-			assert !@car.save
-		end
-	end
-
-	context "car without price" do
-		setup do
-			@car = Car.new @valid_attrs.merge(:price => "")
-		end
-
-		should "not save" do
-			assert !@car.save
-		end
-	end
+	should validate_presence_of :licenceNumber
+	should validate_presence_of :model
+	should validate_presence_of :yearModel
+	should validate_presence_of :price
 end
