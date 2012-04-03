debe89c23d2fa9baa91d0d41570158333686558b
Santtu Lintervo <santervo@gmail.com>
2012-04-03 21:55:18 +0300

    initial rails app w/o active record

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CardealerDemoKo::Application.config.secret_token = 'dcdbf8baeb28dc9569bce4460f04adafeef6f0be07b2073ffab7bd3a43c1a4ac84abf6f5af64319d1f4188af86e49caf933dbcb20aa27fa28e8c805ac3a3f94b'
