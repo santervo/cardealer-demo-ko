480d7f32c8b7a0bf3fe86009a70d44715dcc790c
Santtu Lintervo <santervo@gmail.com>
2012-04-04 23:11:27 +0300

    added sell modal form

--- public/js/cardealer.js
+++ public/js/cardealer.js
@@ -22,7 +22,8 @@ $(document).ready(function() {
         var self = this;
         self.cars = ko.observableArray([
                 new Car("Honda", 2001, "XYZ-123", 3500.0),
-                new Car("Toyota", 2005, "ABC-123", 7000.0, 
+                new Car("Citroen", 2004, "FGE-123", 5000.0),
+                 new Car("Toyota", 2005, "ABC-123", 7000.0, 
                     new SaleContract("Matti Meikäläinen", 6900.0))
                 ]);
         self.vendibleCars = function() {
@@ -31,6 +32,7 @@ $(document).ready(function() {
         self.soldCars = function() {
             return _.filter(self.cars(), function(car) { return car.isSold();});
         };
+        self.carToSell = ko.observable();
     }
 
     ko.applyBindings(new CarDealerViewModel());
