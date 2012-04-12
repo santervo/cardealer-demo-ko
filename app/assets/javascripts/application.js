4b1f713d5a5f8ddfdde46797a24bd91d7c63e436
Santtu Lintervo <santervo@gmail.com>
2012-04-12 22:45:22 +0300

    use knockout.mapping

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,5 +14,6 @@
 //= require jquery_ujs
 //= require_tree .
 //= require knockout
+//= require knockout.mapping
 //= require underscore
 //= require twitter/bootstrap
--- app/assets/javascripts/cardealer.js
+++ app/assets/javascripts/cardealer.js
@@ -1,35 +1,39 @@
 $(document).ready(function() {
-    var SaleContract = function(car, customer, price) {
-        this.car = car;
-        this.customer = ko.observable(customer);
-        this.price = ko.observable(price);
+    var SaleContract = function(data) {
+        ko.mapping.fromJS(data, {}, this);
     };
-    var Car = function(model, yearModel, licenceNumber, price) {
-        this.model = ko.observable(model);
-        this.yearModel = ko.observable(yearModel);
-        this.licenceNumber = ko.observable(licenceNumber);
-        this.price = ko.observable(price);
+    var VendibleCar = function(data) {
+        ko.mapping.fromJS(data, {}, this);
+    };
+    var SaleTransaction = function(vendibleCar) {
+        this.vendibleCar = vendibleCar;
+        this.saleContract = new SaleContract({car: vendibleCar.car, customer: "", price: ""});
     };
 
     var CarDealerViewModel = function() {
         var self = this;
-        self.cars = ko.observableArray([
-                new Car("Honda", 2001, "XYZ-123", 3500.0),
-                new Car("Citroen", 2004, "FGE-123", 5000.0)]);
+        self.vendibleCars = ko.observableArray([
+                new VendibleCar({
+                    car: {model: "Honda", yearModel: 2001, licenceNumber: "XYZ-123"}, 
+                    price: 3500.0}),
+                new VendibleCar({
+                    car: {model: "Citroen", yearModel: 2004, licenceNumber: "FGE-123"}, 
+                    price: 3500.0})]);
         self.saleContracts = ko.observableArray([
-                new SaleContract(new Car("Toyota", 2005, "ABC-123", 7000.0),
-                    "Matti Meikäläinen", 6900.0)]);
-        self.newSaleContract = ko.observable();
-        self.sell = function(car) {
-            self.newSaleContract(new SaleContract(car));
+                new SaleContract({
+                    car: {model: "Toyota", yearModel: 2005, licenceNumber: "ABC-123"}, 
+                    customer: "Matti Meikäläinen", price: 7000.0})]);
+        self.saleTransaction = ko.observable();
+        self.sell = function(vendibleCar) {
+            self.saleTransaction(new SaleTransaction(vendibleCar));
         };
         self.confirm = function() {
-            self.saleContracts.push(self.newSaleContract());
-            self.cars.remove(self.newSaleContract().car);
-            self.newSaleContract(null);
+            self.saleContracts.push(self.saleTransaction().saleContract);
+            self.vendibleCars.remove(self.saleTransaction().vendibleCar);
+            self.saleTransaction(null);
         };
         self.cancel = function() {
-            self.newSaleContract(null);
+            self.saleTransaction(null);
         };
     };
 
