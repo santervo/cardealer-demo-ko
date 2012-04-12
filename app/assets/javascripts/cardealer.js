a136d2e85c573867cf81dd627dc2216d31194aea
Santtu Lintervo <santervo@gmail.com>
2012-04-12 21:49:26 +0300

    use separate models

--- app/assets/javascripts/cardealer.js
+++ app/assets/javascripts/cardealer.js
@@ -1,59 +1,36 @@
 $(document).ready(function() {
-    var SaleContract = function(customer, price, status) {
+    var SaleContract = function(car, customer, price) {
+        this.car = car;
         this.customer = ko.observable(customer);
         this.price = ko.observable(price);
     };
-    var Car = function(model, yearModel, licenceNumber, price, saleContract) {
-        var self = this;
-
+    var Car = function(model, yearModel, licenceNumber, price) {
         this.model = ko.observable(model);
         this.yearModel = ko.observable(yearModel);
         this.licenceNumber = ko.observable(licenceNumber);
         this.price = ko.observable(price);
-        this.saleContract = ko.observable(saleContract);
-        this.preparedContract = ko.observable();
-
-        this.sold = ko.computed(function() {
-            return !!self.saleContract();
-        });
-
-        this.prepared = ko.computed(function() {
-            return !!self.preparedContract();
-        });
-
-        this.sell = function() {
-            self.preparedContract(new SaleContract);
-        };
-
-        this.confirm = function() {
-            self.saleContract(self.preparedContract());
-            self.preparedContract(null);
-        };
-
-        this.revoke = function() {
-            self.preparedContract(null);
-        };
     };
 
     var CarDealerViewModel = function() {
         var self = this;
         self.cars = ko.observableArray([
                 new Car("Honda", 2001, "XYZ-123", 3500.0),
-                new Car("Citroen", 2004, "FGE-123", 5000.0),
-                new Car("Toyota", 2005, "ABC-123", 7000.0, 
-                    new SaleContract("Matti Meikäläinen", 6900.0))
-                ]);
-        self.vendibleCars = function() {
-            return _.filter(self.cars(), function(car) { return !car.sold();});
+                new Car("Citroen", 2004, "FGE-123", 5000.0)]);
+        self.saleContracts = ko.observableArray([
+                new SaleContract(new Car("Toyota", 2005, "ABC-123", 7000.0),
+                    "Matti Meikäläinen", 6900.0)]);
+        self.newSaleContract = ko.observable();
+        self.sell = function(car) {
+            self.newSaleContract(new SaleContract(car));
         };
-        self.soldCars = function() {
-            return _.filter(self.cars(), function(car) { return car.sold();});
+        self.confirm = function() {
+            self.saleContracts.push(self.newSaleContract());
+            self.cars.remove(self.newSaleContract().car);
+            self.newSaleContract(null);
         };
-        self.carToSell = ko.computed(function() {
-            return _.find(self.vendibleCars(), function(car) {
-                return car.prepared();
-            });
-        });
+        self.cancel = function() {
+            self.newSaleContract(null);
+        };
     };
 
     ko.bindingHandlers.showModal = {
